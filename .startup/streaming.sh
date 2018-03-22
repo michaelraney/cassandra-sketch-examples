2bb4edaca52bc3aacb37bd4a04302ac52b39010a
michaelraney <raneymike83@gmail.com>
2018-03-22 19:42:24 -0400

    temp fix for spark resources
    todo add dynamic memory config

--- .startup/streaming.sh
+++ .startup/streaming.sh
@@ -7,13 +7,13 @@ then
   mvn package -f dse-sketching-demo/dse-sketching-streaming-job/pom.xml
 
   #kickoff streaming job
-  nohup dse spark-submit --class com.se.stream.TwitterStreamSketchDemo --total-executor-cores 4 --executor-memory 4g /tmp/datastax-sketch-examples/dse-sketching-demo/dse-sketching-streaming-job/target/dse-sketching-streaming-job-jar-with-dependencies.jar > streaming.log &
+  nohup dse spark-submit --class com.se.stream.TwitterStreamSketchDemo --total-executor-cores 2 --executor-memory 1g /tmp/datastax-sketch-examples/dse-sketching-demo/dse-sketching-streaming-job/target/dse-sketching-streaming-job-jar-with-dependencies.jar > streaming.log &
 
   #write out current crontab for batch
   crontab -l > mycron
 
   #echo new cron into cron file
-  echo "*/3 * * * * dse spark-submit --class com.se.rollup.DataSketchingRollup --total-executor-cores 2 --executor-memory 4g /tmp/datastax-sketch-examples/dse-sketching-demo/dse-sketching-streaming-job/target/dse-sketching-streaming-job-jar-with-dependencies.jar" >> mycron
+  echo "*/3 * * * * dse spark-submit --class com.se.rollup.DataSketchingRollup --total-executor-cores 2 --executor-memory 1g /tmp/datastax-sketch-examples/dse-sketching-demo/dse-sketching-streaming-job/target/dse-sketching-streaming-job-jar-with-dependencies.jar" >> mycron
 
   #install new cron file
   crontab mycron
