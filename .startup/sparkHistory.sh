516103f099a1be2e991b7f62647cbbcf1d3007ff
michaelraney <raneymike83@gmail.com>
2018-04-10 16:02:52 -0400

    history server fix

--- .startup/sparkHistory.sh
+++ .startup/sparkHistory.sh
@@ -12,4 +12,4 @@ set -x
 dse hadoop fs -mkdir /spark
 dse hadoop fs -mkdir /spark/events
 
-sudo nohup dse spark-history-server start --properties-file /tmp/datastax-sketch-examples/dse-sketching-demo/dse-sketching-demo/history-server-config/spark-defaults.conf > spark-history-server.log &
+sudo dse spark-history-server start --properties-file /tmp/datastax-sketch-examples/dse-sketching-demo/history-server-config/spark-defaults.conf > spark-history-server.log
