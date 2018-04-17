843221ccbeada5fd259889be3eced9507e4ba949
michaelraney <raneymike83@gmail.com>
2018-04-17 11:41:57 -0400

    update examples

---
+++ scala/main/src/com/se/stream/TwitterSteamHyperloglogSketch.scala
@@ -0,0 +1,78 @@
+package com.se.stream
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.datastax.spark.connector.{SomeColumns, _}
+import com.twitter.algebird.HyperLogLog._
+import com.twitter.algebird.HyperLogLogMonoid
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import twitter4j.Status
+
+/**
+ *
+ * Created by michaelraney on 3/3/18.
+ */
+class TwitterSteamHyperloglogSketch extends Serializable {
+
+
+    //val tableColumns = SomeColumns("event", "interval", "dimension", "subtotal")
+
+  def start(stream: ReceiverInputDStream[Status], sc: SparkContext, WINDOW_SIZE: Int) {
+
+
+    /**
+     * Example of using HyperLogLog monoid from Twitter's Algebird together with Spark Streaming's
+     * TwitterInputDStream
+     */
+
+    /** Bit size parameter for HyperLogLog, trades off accuracy vs size */
+    val BIT_SIZE = 12
+
+    val users = stream.map(status => status.getUser.getId)
+
+    val hll = new HyperLogLogMonoid(BIT_SIZE)
+
+    val approxUsers = users.mapPartitions(ids => {
+      ids.map(id => hll.create(id))
+    }).reduce(_ + _)
+
+
+    approxUsers.foreachRDD(rdd => {
+
+      val todayAsString = new SimpleDateFormat("MM-dd-yyyy").format(new Date())
+
+      val now = new Date();
+
+      try
+      {
+        val partial = rdd.first()
+
+        val uniqueperbatch = partial.estimatedSize.toInt
+
+        val oneWindowValue = sc.parallelize(Seq(("uniqueusers", todayAsString, now, WINDOW_SIZE, 1, uniqueperbatch, toBytes(partial))))
+
+        oneWindowValue.saveToCassandra("approximations", "hlldata", SomeColumns("id", "date", "batchtime", "batchwindow", "totalinwindow", "uniqueperbatch", "hllstore"))
+
+      }
+      catch
+        {
+          case unknown: Throwable => {
+
+            println(s"Unknown exception: $unknown")
+            val partial = hll.zero;
+            val oneWindowValue = sc.parallelize(Seq(("uniqueusers", todayAsString, now, WINDOW_SIZE, 1, 0, toBytes(partial))))
+            oneWindowValue.saveToCassandra("approximations", "hlldata", SomeColumns("id", "date", "batchtime", "batchwindow", "totalinwindow", "uniqueperbatch", "hllstore"))
+          }
+        }
+      finally
+      {
+        // your scala code here, such as to close a database connection
+      }
+
+
+    })
+
+  }
+}
---
+++ scala/main/src/com/se/stream/TwitterStreamSketchDemo.scala
@@ -0,0 +1,73 @@
+package com.se.stream
+
+/**
+ * Created by michaelraney on 3/2/18.
+ */
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import twitter4j.FilterQuery
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
+
+object TwitterStreamSketchDemo {
+
+    def main(args: Array[String]): Unit = {
+
+        println("=============== TWITTER KEY ============")
+        println(args(0))
+        println(args(1))
+        println(args(2))
+        println(args(3))
+
+        val conf = new SparkConf(true).setAppName("DataSketchingTwitterStream");
+
+        val sc = new SparkContext(conf)
+
+        //Spark Streaming
+        val WINDOW_SIZE = 10
+
+        //Twitter Authentication
+        val cb = new ConfigurationBuilder();
+
+
+        //BYO Credentials
+        cb.setDebugEnabled(true)
+          .setOAuthConsumerKey(args(0))
+          .setOAuthConsumerSecret(args(1))
+          .setOAuthAccessToken(args(2))
+          .setOAuthAccessTokenSecret(args(3))
+
+        val auth = new OAuthAuthorization(cb.build)
+
+
+        //USA rough Bounding Box
+        val matrix = Array.ofDim[Double](2,2)
+        matrix(0)(0) = -124.848974
+        matrix(0)(1) = 24.396308
+        matrix(1)(0) = -66.885444
+        matrix(1)(1) = 49.384358
+
+
+        val tweetFilterQuery = new FilterQuery();
+        tweetFilterQuery.language("en")
+        tweetFilterQuery.locations(matrix : _*) //USA Bounding box
+
+        val ssc = new StreamingContext(sc, Seconds(WINDOW_SIZE))
+
+        //Twitter Stream
+        val stream = TwitterUtils.createFilteredStream(ssc, Some(auth), Some(tweetFilterQuery), StorageLevel.MEMORY_ONLY_SER_2)
+
+        val countMinSketch = new TwitterSteamCountMinSketch
+        val hyperLoglog = new TwitterSteamHyperloglogSketch
+
+        countMinSketch.start(stream, sc)
+        hyperLoglog.start(stream, sc, WINDOW_SIZE)
+
+        ssc.start()
+        //ssc.awaitTerminationOrTimeout(minutesToStream * 60  * 1000)//5 minutes...
+        ssc.awaitTermination()
+        ssc.stop(stopSparkContext = false, stopGracefully=true)
+    }
+}
